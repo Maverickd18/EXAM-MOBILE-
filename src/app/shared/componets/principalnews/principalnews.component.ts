c4c4aa48b493b0cd1175b2a16e8302cc8f6d5435
Maverickd18 <maverickdiazserna@gmail.com>
2025-08-30 00:46:16 -0400

    Add componets with standalone in false

---
+++ src/app/shared/componets/principalnews/principalnews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principalnews',
+  templateUrl: './principalnews.component.html',
+  styleUrls: ['./principalnews.component.scss'],
+  standalone:false
+})
+export class PrincipalnewsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
